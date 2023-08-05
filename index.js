3ed43d31d56c0f2fc7391e0368bab35e47cc2b13
Shubho <71793619+ohShubho@users.noreply.github.com>
2023-08-04 21:00:09 -0400

    Initial Commit - Basic MongoDB CRUD

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5500;
+
+const TodoItemRoute = require('./routes/todoItems');
+
+mongoose.connect(process.env.DB_CONNECT)
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+
+
+
+app.use('/', TodoItemRoute);
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
